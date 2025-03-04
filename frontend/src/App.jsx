c6d3571df89c5dad890f0ad4b86214b252d0234b
Ankit Kumar <drankitkr011@gmail.com>
2025-03-04 10:34:52 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
-import React from "react";
 
-export default function App() {
-  return <div className="bg-red w-full h-50">App</div>;
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+    </>
+  )
 }
+
+export default App
